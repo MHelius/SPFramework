c203fa39bf582905b62d50638d9c78b65310455a
MHelius <137865881@qq.com>
2015-04-03 17:12:33 +0800

    case

--- controller/index.php
+++ controller/index.php
@@ -16,7 +16,21 @@ class index extends View{
 	{
 		$this->assign('test','This is SPF Hello World');
 
-		$this->toView();
+		//$mysql = new Mysql('test','test');
+
+		//$mysql->field(array('*'));
+		//$mysql->where(array('id'=>array(4,5)));
+		//$res = $mysql->fetchAll();
+
+		//$res = $mysql->insert(array('id'=>1,'tips_id'=>1,'a'=>1,'b'=>1));
+
+		//$res = $mysql->update(array('id'=>1),arary('a'=>1));
+
+		//$res = $mysql->delete(array('id'=>1));
+
+		//var_dump();die;
+
+		//$this->toView();
 	}
 
 }
