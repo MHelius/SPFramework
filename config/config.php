62b49b12c4e6e81c5ddde0760272d52263ebe59c
MHelius <137865881@qq.com>
2015-09-02 17:53:07 +0800

    Optimization
    Optimization

--- config/config.php
+++ config/config.php
@@ -18,6 +18,37 @@ return array(
 				\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES'utf8';",
 			)
 		),
+		'test2'=>array(
+			'read'=>array(
+				'20'=>array(
+					'host'  =>'127.0.0.1',
+					'port'  =>'3306',
+					'user'  =>'root',
+					'pass'  =>'',
+					'config'=>array(
+						\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES'utf8';",
+					)
+				),
+				'80'=>array(
+					'host'  =>'127.0.0.1',
+					'port'  =>'3306',
+					'user'  =>'root',
+					'pass'  =>'',
+					'config'=>array(
+						\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES'utf8';",
+					)
+				),
+			),
+			'write'=>array(
+				'host'  =>'127.0.0.1',
+				'port'  =>'3306',
+				'user'  =>'root',
+				'pass'  =>'',
+				'config'=>array(
+					\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES'utf8';",
+				)
+			),
+		),
 	),
 	'redis'=>array(
 		'0'=>array(
