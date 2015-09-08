3d24eb4cb936a6c401d25c519b07d1063a1d0ba0
MHelius <137865881@qq.com>
2015-09-08 12:34:54 +0800

    fix bug
    fix bug

--- core/SFPException.php
+++ core/SFPException.php
@@ -20,9 +20,11 @@ class SFPException {
 		$message    = $exception->getMessage();
 		$line       = $exception->getLine();
 		$file       = $exception->getFile();
+		$trac       = $exception->getTraceAsString();
 
 		echo "<h3>SFP ERROR</h3><h4>$message [错误代码:$code]</h4>";
-		echo "<h5>错误发生在文件：$file 第 $line 行</h5>";
+		echo "<h5>错误发生在文件 $file 第 $line 行</h5>";
+		echo "<pre style='border: 1px dashed #999999; padding: 5px;'>$trac</pre>";
 		echo "<h6>PHP版本： " .PHP_VERSION ." (" .PHP_OS .")</h6>";
 	}
 
